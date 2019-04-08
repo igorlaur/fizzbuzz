b7b9faa007c7b3f9efe74b71035d12989971b7bc
Igor Laur <igor.laur2@gmail.com>
2019-04-08 10:27:18 -0300

    Correção

--- index.js
+++ index.js
@@ -23,14 +23,4 @@ for(i = 1; i <= 100; i++){
 /* Coloquei o múltiplo 3 e 5 antes pois se ele já for múltiplodos dois já imprime
 o fizzbuzz, se eu colocasse lá embaixo ele iria primeiro ler os dois primeiros e 
 após iria imprimir o fizzbuzz, exemplo: 
-
-  if(multiplo3 == 0){ // if 0.33
-    console.log('fizz', i)
-  }
-  else if(multiplo5 == 0){ // if 0.2
-    console.log('buzz', i)
-  }
-  else if(multiplo35 == 0){ 
-    console.log('FizzBuzz', i)
-  }
 */
